46f79c39353ca03f2bb19268223ff65bb2fdb1eb
Madson919 <mihailmilai99@gmail.com>
2025-09-08 00:06:53 +0700

    5.5 Углубление в Playwright. Полезные возможности Playwright

---
+++ playwright_disabled.py
@@ -0,0 +1,15 @@
+from playwright.sync_api import sync_playwright, expect
+
+
+with sync_playwright() as pl:
+    browser = pl.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+
+    login_button = page.get_by_test_id('login-page-login-button')
+    # expect(login_button).to_be_disabled() # disabled
+    # expect(login_button).to_be_enabled() # enabled
+    expect(login_button).not_to_be_disabled() # enabled
+
+    page.wait_for_timeout(5000)
