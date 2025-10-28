a13fb6122ed4d0f80088ef685cabb6d1ff2a03e5
Madson919 <mihailmilai99@gmail.com>
2025-10-28 10:26:57 +0700

    6.5 Фикстуры Pytest

---
+++ tests/test_usefixtures.py
@@ -0,0 +1,23 @@
+import pytest
+
+
+@pytest.fixture
+def clear_books_database() -> None:
+    print("[FIXTURE] Удаляем все данные из базы данных")
+
+
+@pytest.fixture
+def fill_books_database() -> None:
+    print("[FIXTURE] Создаем новые данные в базе данных")
+
+
+@pytest.mark.usefixtures("clear_books_database")
+def test_read_all_books_in_library():
+    print("reading all books")
+
+
+@pytest.mark.usefixtures("clear_books_database", "fill_books_database") # фикстуры вызываются по порядку, указанному в декораторе
+class TestLibrary:
+    def test_read_book_from_library(self): ...
+
+    def test_delete_book_from_library(self): ...
