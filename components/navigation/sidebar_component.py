6b3c313259a0cb4eb02417604b0b239bd41bd5dd
Madson919 <mihailmilai99@gmail.com>
2025-11-24 12:13:19 +0700

    7.3 Знакомство с PageComponent

---
+++ components/navigation/sidebar_component.py
@@ -0,0 +1,28 @@
+import re
+from playwright.sync_api import Page, expect
+
+from components.base_component import BaseComponent
+from components.navigation.sidebar_list_item_component import SidebarListItemComponent
+
+class SidebarComponent(BaseComponent):
+
+    def __init__(self, page: Page) -> None:
+        super().__init__(page)
+
+        self.logout_list_item = SidebarListItemComponent(page, "logout")
+        self.dashboard_list_item = SidebarListItemComponent(page, "dashboard")
+        self.courses_list_item = SidebarListItemComponent(page, "courses")
+
+    def check_visible(self) -> None:
+        self.logout_list_item.check_visible("Logout")
+        self.dashboard_list_item.check_visible("Dashboard")
+        self.courses_list_item.check_visible("Courses")
+
+    def click_logout(self) -> None:
+        self.logout_list_item.navigate(expected_url=re.compile(r".*/#/auth/login"))
+    
+    def click_dashboard(self) -> None:
+        self.dashboard_list_item.navigate(expected_url=re.compile(r".*/#/dashboard"))
+
+    def click_courses(self) -> None:
+        self.courses_list_item.navigate(expected_url=re.compile(r".*/#/courses"))
