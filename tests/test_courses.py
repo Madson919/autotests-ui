880cca3be71605ca8a2dc009c143a56a4c68eb01
Madson919 <mihailmilai99@gmail.com>
2025-11-24 23:41:54 +0700

    7.4 Практикуемся в реализации PageComponent. Реализация и использование компонента CoursesListToolbarViewComponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,5 +1,4 @@
 import pytest
-from playwright.sync_api import expect, Page
 from pages.create_course_page import CreateCoursePage
 from pages.courses_list_page import CoursesListPage
 
@@ -9,8 +8,7 @@ def test_empty_courses_list(courses_list_page: CoursesListPage):
     courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
     courses_list_page.navbar.check_visible(username="username")
     courses_list_page.sidebar.check_visible()
-    courses_list_page.check_visible_courses_title()
-    courses_list_page.check_visible_create_course_button()
+    courses_list_page.toolbar_view.check_visible()
     courses_list_page.check_visible_empty_view()
 
 
@@ -49,8 +47,7 @@ def test_create_course(courses_list_page: CoursesListPage, create_course_page: C
     )
     create_course_page.click_create_course_button()
 
-    courses_list_page.check_visible_courses_title()
-    courses_list_page.check_visible_create_course_button()
+    courses_list_page.toolbar_view.check_visible()
     courses_list_page.course_view.check_visible(
         index=0,
         title="Playwright",
