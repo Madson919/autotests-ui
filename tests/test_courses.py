7db9246d1e577f1d131c11260a7018289d02c333
Madson919 <mihailmilai99@gmail.com>
2025-11-24 12:52:50 +0700

    7.3 Знакомство с PageComponent. Практика работы с Page Component

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -5,23 +5,15 @@ from pages.courses_list_page import CoursesListPage
 
 @pytest.mark.regression
 @pytest.mark.courses
-def test_empty_courses_list(chromium_page_with_state: Page):
-    chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+def test_empty_courses_list(courses_list_page: CoursesListPage):
+    courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+    courses_list_page.navbar.check_visible(username="username")
+    courses_list_page.sidebar.check_visible()
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_empty_view()
 
-    courses_page_title = chromium_page_with_state.get_by_test_id("courses-list-toolbar-title-text")
-    expect(courses_page_title).to_be_visible()
-    expect(courses_page_title).to_have_text("Courses")
 
-    courses_empty_view_icon = chromium_page_with_state.get_by_test_id("courses-list-empty-view-icon")
-    expect(courses_empty_view_icon).to_be_visible()
-
-    courses_empty_title_text = chromium_page_with_state.get_by_test_id("courses-list-empty-view-title-text")
-    expect(courses_empty_title_text).to_be_visible()
-    expect(courses_empty_title_text).to_have_text("There is no results")
-
-    courses_empty_view_description = chromium_page_with_state.get_by_test_id("courses-list-empty-view-description-text")
-    expect(courses_empty_view_description).to_be_visible()
-    expect(courses_empty_view_description).to_have_text("Results from the load test pipeline will be displayed here")
 
 
 @pytest.mark.regression
