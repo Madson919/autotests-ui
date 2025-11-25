e2840ac9749d3d7ce3843337e670b2389064f778
Madson919 <mihailmilai99@gmail.com>
2025-11-26 01:07:15 +0700

    7.4 Практикуемся в реализации PageComponent. Практикуемся в написании компонентов с использованием паттерна PageComponent

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,3 +1,4 @@
+from components.authentication.registration_form_component import RegistrationFormComponent
 from pages.base_page import BasePage
 from playwright.sync_api import Page, expect
 
@@ -7,20 +8,9 @@ class RegistrationPage(BasePage):
     def __init__(self, page: Page) -> None:
         super().__init__(page)
 
-        self.registration_form_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        self.registration_form_username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-        self.registration_form_password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+        self.registration_form = RegistrationFormComponent(page)
+
         self.registration_button = page.get_by_test_id('registration-page-registration-button')
-
-    def fill_registration_form(self, email: str, username: str, password: str) -> None:
-        self.registration_form_email_input.fill(email)
-        expect(self.registration_form_email_input).to_have_value(email)
-
-        self.registration_form_username_input.fill(username)
-        expect(self.registration_form_username_input).to_have_value(username)
-
-        self.registration_form_password_input.fill(password)
-        expect(self.registration_form_password_input).to_have_value(password)
 
     def click_registration_button(self) -> None:
         self.registration_button.click()
