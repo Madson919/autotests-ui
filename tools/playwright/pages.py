70d3ed904578409d621705b64d80b1d047250abc
Madson919 <mihailmilai99@gmail.com>
2025-12-05 12:28:50 +0700

    9.4 Моки и оптимизация UI автотестов

--- tools/playwright/pages.py
+++ tools/playwright/pages.py
@@ -1,6 +1,8 @@
 import allure
 from playwright.sync_api import Playwright, Page, Browser
 from config import settings
+from tools.playwright.mocks import mock_static_resources
+
 
 def initialize_playwright_page(
         playwright: Playwright,
@@ -16,6 +18,7 @@ def initialize_playwright_page(
     )
     context.tracing.start(screenshots=True, snapshots=True, sources=True)
     page = context.new_page()
+    mock_static_resources(page)
 
     yield page
 
